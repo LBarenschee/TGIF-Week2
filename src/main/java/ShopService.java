c1523f1ba8cfc882f631a308502cff06ce571042
LBarenschee <barenschee@gmail.com>
2022-04-01 14:54:01 +0200

    Prepared methods in ShopService class

--- src/main/java/ShopService.java
+++ src/main/java/ShopService.java
@@ -1,4 +1,26 @@
 public class ShopService {
 
+    ProductRepo productRepo;
+    OrderRepo orderRepo;
 
+    public Product getProduct(){
+        return null;
+    }
+
+    public void listProduct(){
+    //sout planned
+    }
+
+    public void addOrder(){
+
+    }
+
+    public Order getOrder(int id){
+        return null;
+    }
+
+    public void listOrder(){
+
+    }
 }
+
