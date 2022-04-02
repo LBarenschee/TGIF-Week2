668f949e7103ec604b53616458dd37a701d4b3fc
Stefan Bollmann <stefan.bollmann@rwth-aachen.de>
2022-04-02 08:11:09 +0200

    User menu: Workflow is - User chooses if he wants to make a new order or list all orders (or exit - however he does not know that he needs to type '-1'). - User makes order or gets a list on stdout.
    tbd: error handling, menu refactoring, general refactoring.

--- src/main/java/ShopService.java
+++ src/main/java/ShopService.java
@@ -1,9 +1,6 @@
-import java.util.ArrayList;
-import java.util.Map;
+import java.util.*;
 
 public class ShopService {
-
-
     private ProductRepo productRepo;
     private OrderRepo orderRepo;
 
@@ -18,12 +15,12 @@ public class ShopService {
     }
 
     public String listProduct(){
-        String finalString = "";
+        StringBuilder finalString = new StringBuilder();
         ArrayList<Product> products = productRepo.list();
         for (Product product : products) {
-            finalString += "[" + product.getId() + "] " + product.getName() +"\n";
+            finalString.append("[").append(product.getId()).append("] ").append(product.getName()).append("\n");
         }
-        return finalString;
+        return finalString.toString();
     }
 
     public String addOrder(Order order){
@@ -44,13 +41,80 @@ public class ShopService {
         String finalString = "";
         ArrayList<Order> orders = orderRepo.list();
         for(Order order : orders) {
-            finalString += "[" + order.getId() + "] " + order.toString() +"\n";
+            finalString += "[" + order.getId() + "] " + order +"\n";
         }
         return finalString;
     }
 
+    public void makeNewOrderMenu(){
+            System.out.println(listProduct());
+            System.out.println("Which products do you want to order?");
+            System.out.println("Press -1 <Enter> when done.");
+            int product2order = 0;
+            List<Product> productList = new ArrayList<>();
+            while (product2order != -1) {
+                Scanner userInput = new Scanner(System.in);
+                product2order = userInput.nextInt();
+                if (product2order != -1) {
+                    productList.add(getProduct(product2order));
+                }
+            }
+            this.addOrder(new Order(1, productList));
+    }
+
     public void userMenu(){
         //handle user choice of action
+        int action = 0;
+        while(action != -1) {
+            switch (action){
+                case 0:
+                    break;
+                case 1:
+                    makeNewOrderMenu();
+                    break;
+                case 2:
+                    listAllOrdersMenu();
+                    break;
+            }
+            showAvailableActions();
+            Scanner userInput = new Scanner(System.in);
+            action = userInput.nextInt();
+        }
+    }
+
+    private void listAllOrdersMenu() {
+        System.out.println(orderRepo.list());
+    }
+
+    private void showAvailableActions() {
+        String availableActionsMenu = "";
+        availableActionsMenu = "Which action do you want to do?\n";
+        availableActionsMenu += "[1] Select products and order them.\n";
+        availableActionsMenu += "[2] List all orders.\n";
+        System.out.println(availableActionsMenu);
+    }
+
+    static Map fillMap(Map productMap){
+        Product shampoo = new Product(1, "Apfelschampuh");
+        Product soap = new Product(2, "Kernseife");
+        Product milk = new Product(3, "Die gute Bioweidemilch");
+        Product cheese = new Product(4, "Stinkekäse");
+        Product beer = new Product(5, "Tegernseer");
+        productMap.put(shampoo.getId(),shampoo);
+        productMap.put(soap.getId(),soap);
+        productMap.put(milk.getId(),milk);
+        productMap.put(cheese.getId(),cheese);
+        productMap.put(beer.getId(),beer);
+        return productMap;
+    }
+
+    public static void main(String[] args) {
+        Map<Integer, Product> prodmap = new HashMap<>();
+        prodmap = fillMap(prodmap);
+        ProductRepo prodre = new ProductRepo(prodmap);
+        ShopService myShop = new ShopService(prodre);
+        myShop.userMenu();
+        System.out.println(myShop.listOrder());
     }
 }
 
--- src/test/java/ShopServiceTest.java
+++ src/test/java/ShopServiceTest.java
@@ -1,5 +1,3 @@
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.util.*;
@@ -73,7 +71,7 @@ class ShopServiceTest {
 
         Order order = new Order(1, myProducts.list());
 
-        String isAdded = myShopService.addOrder(order);
+        myShopService.addOrder(order);
         assertEquals(order, myShopService.getOrder(1));
     }
 
